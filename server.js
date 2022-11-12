88512caf79dc99bd2d022aaf4f02c5297b7535ea
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-13 01:55:20 +0530

    commonjs moduling patter

---
+++ server.js
@@ -0,0 +1,13 @@
+const employee = require("./employee.js");
+const student = require("./student.js");
+
+
+employee.printName("Harshit Raj");
+employee.printAddress("Prabhunath Nagar, Chapra")
+
+
+
+
+student.studentName("Manikesh Ranjan");
+student.studentMarks("90%");
+
